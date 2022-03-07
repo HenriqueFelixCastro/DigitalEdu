614c6a6c910513c4b34f81b80655f8b3385ffb04
Natalia Francisca dos Santos Gomes <natalia.francisca@ibm.com>
2022-03-07 15:45:00 -0300

    adding new postblog

--- src/database/blogPosts.js
+++ src/database/blogPosts.js
@@ -1,55 +1,48 @@
 export const BLOG_POSTS = [
   {
-    id: "1",
-    title: "Blog Post Example 1",
+    id: "0",
+    title: "New year, new collaboration goals between IBM and  Universities.",
     authors: [
-      { name: "Author 1", title: "Author 1 Title" },
-      { name: "Author 2", title: "Author 2 Title" },
+      { name: "Alcely Strutz Barroso", title: "University Programme Leader" },
     ],
     tags: [
-      { text: "Hard Skills", color: "green" },
+      { text: "Soft Skills", color: "blue" },
       { text: "Trend", color: "cyan" },
     ],
-    date: "07 Jul 2021",
-    duration: "50 minutes",
+    date: "22 Feb 2022",
+    duration: "10 minutes",
     imageURL: `${process.env.PUBLIC_URL}/img/highlited_card.jpg`,
     isJobMarketHighlight: true,
     paragraphsBeforeHighlight: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat2.",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat3.",
-    ],
-    highlight:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaH.",
-    paragraphsAfterHighlight: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat4.",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat5.",
+      "Dear academic community,",
+      "I wish you are having a nice time.",
+      "The last years the education sector has been challenged. Higher education has been pushed to innovate by using digital platforms, hybrid learning experience, and to add technology content in all courses to prepare the workforce the market is looking for.",
+      "We appreciate your partnership and the feedback we have received to create and to adapt the content and format to better assist faculties and universities to implement their value proposition for the students. More than 550 universities in Latin America have actively participated of the IBM Global University Programs, which bring together the experience of the technology industry with the expertise of the academia.",
+      "I am delighted to introduce you to this website renewed according to your feedback. I hope you can enjoy, participate, and continue to provide feedback so we can prepare more useful content for the professors to use in their classes.",
+      "Stay tuned in our virtual trainings, events, and get involved with the Latin America University Alliance to meet researchers, to receive updates about new technologies, and to access case studies. Together, as a trustful community we can innovate and make a difference in the lives of thousands of students.",
+      "Thank you",
+      "Alcely"
+    ]
+  },
+
+  {
+    id: "1",
+    title: "Boosting talent: The collaboration between EGADE Business School and the IBM Academic Initiative.",
+    authors: [{ name: "Jacob Escobar", title: "Director of the Master in Business Analytics", subtitle: "EGADE Business School Tecnológico de Monterrey" }],
+    tags: [{ text: "Career Skills", color: "purple" }],
+    date: "3 Feb 2022",
+    duration: "12 minutes",
+    imageURL: `${process.env.PUBLIC_URL}/img/highlited_card.jpg`,
+    isJobMarketHighlight: false,
+    paragraphsBeforeHighlight: [
+      "The Master in Business Analytics at EGADE Business School arises with the goal of training future professionals who will be the agents of change in organizations, by combining strategic business thinking and the management of exponential technologies to innovate in the industry through data analytics.",
+      "The alliance between EGADE Business School and the IBM Academic Initiative has become an important element to achieve the development of these skills in the students of the Master in Business Analytics. Our students' access to the IBM Skills Academy and IBM Cloud digital platforms allows them to incorporate high-value content into courses and have access to academic content, labs, and industry use cases focused on solving real business problems. These contents also give access to certifications with digital badges highly valued in the labor market and recognized worldwide. In addition to this digital content, our strategic alliance with IBM has also led us to collaborate through live sessions with IBM experts on Digital Transformation, Design Thinking, AI, Blockchain, IoT, Cloud Computing, Cybersecurity, Quantum Computers and others.",
+      "The Master's program in Business Analytics seeks to develop in students a disruptive mindset based on data science and foster leadership to accelerate digital transformation in organizations, which is why the alliance with the IBM Academic Initiative gives added value to our students through access to cutting-edge content in the professional field and linkages with the industry.",
     ],
   },
 
   {
     id: "2",
-    title: "Blog Post Example 2",
-    authors: [{ name: "Author 1", title: "Author 1 Title" }],
-    tags: [{ text: "Career Skills", color: "purple" }],
-    date: "05 Jul 2021",
-    duration: "12 minutes",
-    imageURL: `${process.env.PUBLIC_URL}/img/highlited_card.jpg`,
-    isJobMarketHighlight: false,
-    paragraphsBeforeHighlight: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat2.",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat3.",
-    ],
-    highlight:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaH.",
-    paragraphsAfterHighlight: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat4.",
-    ],
-  },
-
-  {
-    id: "3",
     title: "Blog Post Example 3",
     authors: [
       { name: "Author 1", title: "Author 1 Title" },

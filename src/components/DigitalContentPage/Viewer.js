850ee6efbba5f8528e38447f89e28f2d4efa2dfb
Natalia Francisca dos Santos Gomes <natalia.francisca@ibm.com>
2022-03-08 11:00:25 -0300

    organizing the style

--- src/components/DigitalContentPage/Viewer.js
+++ src/components/DigitalContentPage/Viewer.js
@@ -4,65 +4,61 @@ import { Context } from './Context';
 import { Close24 } from "@carbon/icons-react";
 import { Button } from "carbon-components-react";
 
-function Viewer() {
+const stylesheet = {
+  componentViewerVideoDetails: {
+    width: '100%',
+    heigth: '20%',
+    borderBottom: 'solid rgba(255,255,255, .9) 2px',
+    padding: '0 1rem',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+
+  title: {
+    textAlign: 'center',
+    color: '#fff',
+    font: "normal normal 600 14px/34px IBM Plex Sans",
+  },
+}
+
+const Viewer = () => {
 
     const {dataVideo, visibleViewer, setVisibleViewer, topValue} = useContext(Context);
     const {videoTitle, videoUrl} = dataVideo;
 
     const iframeRef = useRef("");
 
-    const stylesheet = {
-        componentViewerVideo: {
-            background: 'black',
-            color: 'white',
-
-            position: 'absolute',
-
-            top: `${topValue + 195}px`,
-            zIndex: '111',
-            border: 'solid rgba(255,255,255, .9) 2px',
-
-            visibility: `${visibleViewer}`,
-
-            display: 'flex',
-            flexDirection: 'column',
-
-            width: '90%',
-            height: '95vh',
-            left: "5%"
-        },
-
-        componentViewerVideoDetails: {
-            width: '100%',
-            heigth: '20%',
-            borderBottom: 'solid rgba(255,255,255, .9) 2px',
-            padding: '0 1rem',
-
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-        },
-
-        title: {
-            textAlign: 'center',
-            color: '#fff',
-            font: "normal normal 600 14px/34px IBM Plex Sans",
-        },
-    }
-
     const closeViewerVideo = () => {
-        // QUANDO A TELA FECHAR, O VÍDEO VAI PARAR TAMBÉM
         iframeRef.current.src = ''
         iframeRef.current.src = videoUrl
         setVisibleViewer("hidden");
     }
 
-    const videoReady = () => {
-        console.log("tá pronto...")
+    const stylesheetViewerVideo = {
+      background: 'black',
+      color: 'white',
+  
+      position: 'absolute',
+  
+      top: `${topValue + 195}px`,
+      zIndex: '111',
+      border: 'solid rgba(255,255,255, .9) 2px',
+  
+      visibility: `${visibleViewer}`,
+  
+      display: 'flex',
+      flexDirection: 'column',
+  
+      width: '90%',
+      height: '95vh',
+      left: "5%"
     }
 
+    
     return (
-        <div style={stylesheet.componentViewerVideo} className="component-viewer-video">
+        <div style={stylesheetViewerVideo} className="component-viewer-video">
             <div style={stylesheet.componentViewerVideoDetails}>
                 <h1 style={stylesheet.title}>{videoTitle}</h1>
                 <Button 
@@ -83,7 +79,6 @@ function Viewer() {
                 allowFullScreen
                 webkitallowfullscreen
                 frameBorder="0"
-                onLoad={videoReady()}
             /> 
         </div>
     )
